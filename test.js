dc4cc0e0f019889e3073a6516beb45b788bc8976
marcelle-mann <95marcelle.mann@gmail.com>
2024-03-08 12:06:28 +0000

    First commit

---
+++ test.js
@@ -0,0 +1,5 @@
+import MarcelleMannt from './index.js';
+
+const getEth = new MarcelleMannt()
+getEth.getBalance("0x65DB1a5ac48d4A97dC0A104D8276d7017208A975",).then(balance => { console.log("Balance without lib: ", balance) })
+getEth.getBalance("0x65DB1a5ac48d4A97dC0A104D8276d7017208A975", "GetEthBalance").then(balance => { console.log("Balance with lib: ", balance) })
